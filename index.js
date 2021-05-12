26f8ca2ebfb5931a9b3c01905034d2b5da71de06
konstantin krivlenia <krivlenia@gmail.com>
2021-05-12 14:28:37 +0300

    fix lint error

--- index.js
+++ index.js
@@ -34,61 +34,60 @@ const ignorePredicate = (skipNames, value) => {
   );
 };
 
-module.exports = stylelint.createPlugin(ruleName, function ({
-  cssDefinitions = [],
-  ignoreProperties,
-  ignoreValues,
-}) {
-  return function (postcssRoot, postcssResult) {
-    if (cssDefinitions.length === 0) {
-      return;
-    }
-    postcssRoot.walkDecls(function (decl) {
-      // ignore properties from ignore list
-      if (
-        ignorePredicate(ignoreProperties, decl.prop) ||
-        ignorePredicate(ignoreValues, decl.value)
-      ) {
+module.exports = stylelint.createPlugin(
+  ruleName,
+  function ({ cssDefinitions = [], ignoreProperties, ignoreValues }) {
+    return function (postcssRoot, postcssResult) {
+      if (cssDefinitions.length === 0) {
         return;
       }
-      let ast;
+      postcssRoot.walkDecls(function (decl) {
+        // ignore properties from ignore list
+        if (
+          ignorePredicate(ignoreProperties, decl.prop) ||
+          ignorePredicate(ignoreValues, decl.value)
+        ) {
+          return;
+        }
+        let ast;
 
-      try {
-        ast = csstree.parse(decl.value, {
-          context: "value",
-        });
-      } catch (e) {
-        return stylelint.utils.report({
-          message: messages.parseError(decl.value),
-          node: decl,
-          result: postcssResult,
-          ruleName: ruleName,
-        });
-      }
-      cssDefinitions.forEach((cssDefinition) => {
-        const fragments = csstree.lexer.findValueFragments(
-          decl.prop,
-          ast,
-          "Type",
-          cssDefinition
-        );
-        if (fragments.length) {
-          const message = messages.unexpectedDecl(
-            decl.toString(),
-            cssDefinition
-          );
-
-          stylelint.utils.report({
-            message: message,
+        try {
+          ast = csstree.parse(decl.value, {
+            context: "value",
+          });
+        } catch (e) {
+          return stylelint.utils.report({
+            message: messages.parseError(decl.value),
             node: decl,
             result: postcssResult,
             ruleName: ruleName,
           });
         }
+        cssDefinitions.forEach((cssDefinition) => {
+          const fragments = csstree.lexer.findValueFragments(
+            decl.prop,
+            ast,
+            "Type",
+            cssDefinition
+          );
+          if (fragments.length) {
+            const message = messages.unexpectedDecl(
+              decl.toString(),
+              cssDefinition
+            );
+
+            stylelint.utils.report({
+              message: message,
+              node: decl,
+              result: postcssResult,
+              ruleName: ruleName,
+            });
+          }
+        });
       });
-    });
-  };
-});
+    };
+  }
+);
 
 module.exports.ruleName = ruleName;
 module.exports.messages = messages;
